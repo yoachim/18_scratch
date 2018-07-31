e6cf7e8105d315a854cb6bcd31dda1f4e340b68e
Peter Yoachim <yoachim@uw.edu>
2018-07-31 11:03:47 -0700

    trying out docker again

---
+++ docker_test/docker_opsim.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+run_dir=/Users/yoachim/gitRepos/18_scratch/docker_test/run_dir
+config_dir=/Users/yoachim/gitRepos/18_scratch/docker_test/config_dir
+sky_brightness_data_dir=$HOME/gitRepos/sims_skybrightness_pre/data
+host_ip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+
+docker run -it --rm --name "$1" \
+          -v ${run_dir}:/home/opsim/run_local \
+          -v ${config_dir}:/home/opsim/other-configs \
+          -v ${sky_brightness_data_dir}:/home/opsim/repos/sims_skybrightness_pre/data \
+          -v $HOME/.config:/home/opsim/.config \
+          -e OPSIM_HOSTNAME=${host_name} \
+          -e DISPLAY=${host_ip}:0 \
+          -p 8888:8888 \
+          oboberg/opsim4_fbs_py3:latest
